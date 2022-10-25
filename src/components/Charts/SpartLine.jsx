ab82c966f34a1957643b127595dcd0b9c07f3c7c
Battuvshin6 <97160313+Battuvshin6@users.noreply.github.com>
2022-10-25 21:27:11 +0800

    [Sidebar added]

---
+++ src/components/Charts/SpartLine.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SpartLine = () => {
+  return <div>SpartLine</div>;
+};
+
+export default SpartLine;
---
+++ src/pages/Customers.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Customers = () => {
+  return <div>s</div>;
+};
+
+export default Customers;
