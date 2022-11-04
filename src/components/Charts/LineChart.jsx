9cfab1c82ed494cc3f318fbadebbf44aaedbb2b4
Battuvshin6 <97160313+Battuvshin6@users.noreply.github.com>
2022-11-04 10:38:57 +0800

    [DONE]

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,22 +1,12 @@
-import React from "react";
-import {
-  ChartComponent,
-  SeriesCollectionDirective,
-  SeriesDirective,
-  Inject,
-  DateTime,
-  Legend,
-  Tooltip,
-  LineSeries,
-} from "@syncfusion/ej2-react-charts";
-import {
-  lineCustomSeries,
-  LinePrimaryYAxis,
-  LinePrimaryXAxis,
-} from "../../data/dummy";
-import { useStateContext } from "../../context/ContextProvider";
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+
 const LineChart = () => {
   const { currentMode } = useStateContext();
+
   return (
     <ChartComponent
       id="line-chart"
@@ -25,13 +15,13 @@ const LineChart = () => {
       primaryYAxis={LinePrimaryYAxis}
       chartArea={{ border: { width: 0 } }}
       tooltip={{ enable: true }}
-      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      legendSettings={{ background: 'white' }}
     >
-      <Inject services={[Legend, DateTime, LineSeries, Tooltip]} />
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
       <SeriesCollectionDirective>
-        {lineCustomSeries.map((item, index) => (
-          <SeriesDirective key={index} {...item} />
-        ))}
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {lineCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
       </SeriesCollectionDirective>
     </ChartComponent>
   );
